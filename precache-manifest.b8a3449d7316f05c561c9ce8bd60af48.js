f79112e110dca6e70ed9547c75e77d27906c3c1c
friend-code-dev <pete.olson.enve@gmail.com>
2022-09-11 00:24:57 -0500

    Updates

---
+++ precache-manifest.b8a3449d7316f05c561c9ce8bd60af48.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "64a8eaab73f8d832ebf0",
+    "url": "/sample-progressive-web-app/static/js/main.64a8eaab.chunk.js"
+  },
+  {
+    "revision": "d9cdb5e30de92556c30a",
+    "url": "/sample-progressive-web-app/static/js/1.d9cdb5e3.chunk.js"
+  },
+  {
+    "revision": "bd8d6acd6c84f5f75961",
+    "url": "/sample-progressive-web-app/static/js/runtime~main.bd8d6acd.js"
+  },
+  {
+    "revision": "d928cf6a051b678ae73c91d0d813e69a",
+    "url": "/sample-progressive-web-app/index.html"
+  }
+];
